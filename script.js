07176400e4de670e25aeb8b6efe4541e1e32683e
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2025-04-26 21:40:25 -0300

    function to add new item and function to remove

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
 const newItem = document.getElementById("new-item");
 const addButton = document.getElementById("add-button");
+const shoppingList = document.getElementById("shopping-list")
+let listItens = ""
 
+addButton.addEventListener("click", (e) => {
+  e.preventDefault()
+  
+  makingList(newItem.value)
+  clearInput()
+  
 
-addButton.addEventListener("click", () => {
-  const item = newItem.value
-
-  console.log(item)
+  console.log(listItens)
 });
+
+shoppingList.addEventListener("click", deleteItem)
+
+function makingList(newItem){
+  let item = `<li>
+            <div class="select-item">
+              <div class="checkmark"></div>
+              <input class="checkbox" type="checkbox">
+              <p>${newItem}</p>
+            </div>
+            <img class="deleteButton" src="./assets/delete.svg" alt="">
+          </li>`  
+
+  listItens = listItens + item
+
+  shoppingList.innerHTML = listItens
+  
+}
+
+function clearInput(){
+  newItem.value = ""
+}
+
+function deleteItem(index){
+  if (event.target.classList.contains("deleteButton")) { 
+    const item = event.target.closest('li'); // acha o <li> mais próximo
+    if (item) {
+      item.remove(); // remove o <li>
+    }
+  }
+}
 
